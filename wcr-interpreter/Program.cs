6906e912d059bd16071d3a2f1c6f3c85d8112e87
Kane <95705546+kaneran@users.noreply.github.com>
2024-04-01 18:56:44 +0100

    Created Repl (Read, eval, print, loop) function

--- wcr-interpreter/Program.cs
+++ wcr-interpreter/Program.cs
@@ -1,21 +1,7 @@
-﻿Console.WriteLine("Hello, please input some wcr code and then press the Enter key");
+﻿using wcr_interpreter;
 
 
-int x = 2;
+var repl = new Repl();
 
-switch (x)
-{
-    case -1:
-        Console.WriteLine("Yes");
-        break;
-    default:
-        if (x == 0) Console.WriteLine("Wow");
-        else Console.WriteLine("Nope");
-        break;
-}
-
-
-var Ch = '=';
-var isWhiteSpace = Ch.Equals(' ') || Ch.Equals('\n') || Ch.Equals('\r') || Ch.Equals('\t');
-
-Console.WriteLine(isWhiteSpace);
+Console.WriteLine("Please enter some commands");
+repl.Start();
---
+++ wcr-interpreter/Repl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wcr_interpreter
+{
+    public class Repl
+    {
+        public void Start() { 
+            var line = Console.ReadLine();
+            var lexer = new Lexer(line);
+
+            var tok = lexer.Next();
+            while(tok.Type != TokenType.EOF) {
+                Console.WriteLine("Type - {0} , Literal - {1}", tok.Type, tok.Literal);
+                tok = lexer.Next();
+            }
+        }
+    }
+}
