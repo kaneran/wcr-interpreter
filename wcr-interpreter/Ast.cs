2756fc874d239c2a394012344b38fbf6f5444eb7
Kane <95705546+kaneran@users.noreply.github.com>
2024-04-02 19:13:55 +0100

    Created structs Program, LetStatement and Identifier to represent the AST (abstract syntax tree)

---
+++ wcr-interpreter/Ast.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wcr_interpreter
+{
+    public class Ast
+    {
+        struct Program {
+            public Statement[] Statements { get; }
+        }
+
+        string TokenLiteral(Program p)
+        {
+            if (p.Statements.Length > 0)
+            {
+                return p.Statements[0].TokenLiteral();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public struct LetStatement : Statement {
+            public Token Token { get; }
+            public Identifier Name { get; }
+            public Expression Expression { get; }
+
+            public void StatementNode()
+            {
+
+            }
+
+            public string TokenLiteral() => Token.Literal;
+        }
+
+
+        public struct Identifier : Expression
+        {
+            public Token Token { get; }
+            public string Value { get; }
+
+            public void ExpressionNode()
+            {
+
+            }
+
+            public string TokenLiteral() => Token.Literal;
+        }
+
+    }
+}
---
+++ wcr-interpreter/Expression.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wcr_interpreter
+{
+    public interface Expression : Node
+    {
+        void ExpressionNode();
+    }
+}
